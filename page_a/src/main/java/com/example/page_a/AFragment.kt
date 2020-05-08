3b9311cd1ba63b50e10cc4c8f9def5cbb4d6692d
Ian Lin <ian.lin@yjtech.tw>
2020-05-08 17:46:25 +0800

    [feature] add module(commonutils, page_a)

---
+++ page_a/src/main/java/com/example/page_a/AFragment.kt
@@ -0,0 +1,32 @@
+package com.example.page_a
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class AFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = AFragment()
+    }
+
+    private lateinit var viewModel: AViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.a_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(AViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ page_a/src/main/java/com/example/page_a/AViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.page_a
+
+import androidx.lifecycle.ViewModel
+
+class AViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
